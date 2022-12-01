8483102af439699eba1274f19801621f35c52c36
Your Name <you@example.com>
2022-12-01 19:31:18 +0530

    Third commit

---
+++ src/Javapractice/SecondOne.java
@@ -0,0 +1,11 @@
+package Javapractice;
+
+public class SecondOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Adding ATM mini project");
+
+	}
+
+}
