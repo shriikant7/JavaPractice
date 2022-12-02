e970f122b1e29e7747153417bdcf934d49320b38
Your Name <you@example.com>
2022-12-02 11:43:56 +0530

    Loops

--- src/Javapractice/FirstOne.java
+++ src/Javapractice/FirstOne.java
@@ -4,7 +4,21 @@ public class FirstOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("h");
+		int i;
+		int j=9;
+		int k=5;
+		for(i=0; i<=4;i++) {
+			System.out.println(i);
+		}
+		while( j<=12) {
+			System.out.println(j);
+			j++;
+		}
+		do{
+		System.out.println(k);	
+		k++;
+		}
+		while(k<=8);
 
 	}
 
